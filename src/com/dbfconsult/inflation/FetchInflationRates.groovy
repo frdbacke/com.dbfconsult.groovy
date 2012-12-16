343acdbee7b7512d3dccb0bc8fe7d1c48b03ccb5
Frederik De Backer <frdbacke@frdbacke-Precision-M6500.(none)>
2012-12-16 21:56:27 +0100

    add R history file

--- src/com/dbfconsult/inflation/FetchInflationRates.groovy
+++ src/com/dbfconsult/inflation/FetchInflationRates.groovy
@@ -3,11 +3,13 @@ package com.dbfconsult.inflation
 class FetchInflationRates {
 
 	static main(args) {
-		def years = 1956..2012;
+		def years = 1962..2012;
 		def pattern = ~/.*<span id="lbl_inflation_average" style="font-weight:bold;">(\d+\.\d+)&nbsp;%<\/span>.*/
 
 		years.each() {
-			def url = "http://www.inflation.eu/inflation-rates/belgium/historic-inflation/cpi-inflation-belgium-${it}.aspx";
+//			def url = "http://www.inflation.eu/inflation-rates/belgium/historic-inflation/cpi-inflation-belgium-${it}.aspx";
+			def url = "http://www.inflation.eu/inflation-rates/the-netherlands/historic-inflation/cpi-inflation-the-netherlands-${it}.aspx";
+			
 			def data = new URL(url).getText()
 
 			data.eachLine { line ->
